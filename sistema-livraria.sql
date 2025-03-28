790144ae67194b218d147e942093695f14913681
nick6328 <nicoly.kozar@escola.pr.gov.br>
2025-03-28 11:15:16 -0300

    Create sistema-livraria.sql

---
+++ sistema-livraria.sql
@@ -0,0 +1,20 @@
+-- Criar o banco de dados
+CREATE DATABASE Livraria;
+
+-- Usar o banco de dados recém-criado
+USE Livraria;
+
+-- Criar a tabela Clientes
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY, -- Chave primária
+    nomeCliente VARCHAR(255), -- Nome do cliente
+    emailCliente VARCHAR(255) -- Email do cliente
+);
+
+-- Criar a tabela Compras
+CREATE TABLE Compras (
+    CompraID INT PRIMARY KEY, -- Chave primária
+    ClienteID INT, -- Chave estrangeira
+    NomeLivro VARCHAR(255), -- Nome do livro
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID) -- Referencia Clientes.ID
+);
